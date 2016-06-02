a06f1251952b2c3a1e4b9f3c90d0975ffb9ca8a7
yao.ni <yao.ni@ele.me>
2016-06-02 14:55:22 +0800

    feat: model extension

---
+++ Swift/NYModel.swift
@@ -0,0 +1,53 @@
+//
+//  NYModel.swift
+//  NYHealthy
+//
+//  Created by NiYao on 5/20/16.
+//  Copyright © 2016 SuneBearNi. All rights reserved.
+//
+
+import Foundation
+
+extension NSObject {
+    
+    class func getClassPropertyNames() -> Array<String> {
+        var properties: Array<String> = []
+        var count: u_int = 0
+        let propertyList = class_copyPropertyList(self, &count)
+        
+        var i = 0
+        while i < Int(count) {
+            let propertyName = property_getName(propertyList[i])
+            let nameString = NSString(UTF8String: propertyName) as! String
+            properties.append(nameString)
+            i += 1
+        }
+        return properties
+    }
+    
+    class func modelObject(dictionary: Dictionary<String, AnyObject>) -> AnyObject {
+        let modelObject = self.init()
+        let propertyMapper = self.propertyMapper() as NSDictionary
+        
+        for key in dictionary.keys {
+           let modelKey = ((propertyMapper.valueForKey(key)) != nil) ? propertyMapper.valueForKey(key) : key
+            modelObject.setValue((dictionary as NSDictionary).objectForKey(key), forKey: modelKey as! String)
+        }
+        return modelObject
+    }
+    
+    class func propertyMapper() -> Dictionary<String, AnyClass> {
+        return [:]
+    }
+    
+    func dictionary() -> Dictionary<String, AnyObject> {
+        var dictionary: Dictionary<String, AnyObject> = [:]
+        let allProperties = self.classForCoder.getClassPropertyNames()
+        var i = 0
+        while i < allProperties.count {
+            dictionary[allProperties[i]] = self.valueForKey(allProperties[i])
+            i += 1
+        }
+        return dictionary
+    }
+}
